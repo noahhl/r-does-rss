fefbd0ea5922d91d95f987f53a02c2a00f762f60
Noah Lorang <noah@noahhl.com>
2011-03-01 17:22:35 -0500

    Unify output from atom and rss

--- R/rss.R
+++ R/rss.R
@@ -24,6 +24,8 @@ parseAtom <- function(content) {
   results <- list()
   results$header <- lapply(content$children$feed[names(content$children$feed) != "entry"], xmlToList)
   results$items <- lapply(content$children$feed[names(content$children$feed) == "entry"], xmlToList)
+  results$items <- lapply(results$items, AtomToRSS)
+  names(results$items) <- rep("item", length(results$items))
   return(results)
 }
 
@@ -32,9 +34,36 @@ parseRSS <- function(content) {
   results <- list()
   results$header <- channel[names(channel) != "item"]
   results$items <- channel[names(channel) == "item"]
-
+  results$items <- lapply(results$items, cleanRSS)
+  names(results$items) <- rep("item", length(results$items))
   return(results)
 
+}
+
+AtomToRSS <- function(atom) {
+  item <- list()
+  item$title <- atom$title
+  item$creator <-   atom$author
+  item$description <- atom$summary
+  item$link <- atom$origlink
+  for (i in atom[grep("link", names(atom))]) {
+    if (i["rel"] == "replies")
+      item$comments <-i["href"]
+  }
+  for (i in atom[grep("category", names(atom))]) {
+      item$categories <- paste(item$categories, i['term'], sep =";")
+  }
+  item$categories <- sub(";", "", item$categories)
+  item$guid <- atom$id
+  item$pubDate <- as.POSIXlt(atom$published)
+  item$source <-  atom$content$.attrs['base']
+  return(item)
+}
+
+cleanRSS <- function(rss) {
+  tryCatch(rss$pubDate <- as.POSIXlt(rss$pubDate), error=function(e) {rss$pubDate <<- as.POSIXlt(rss$pubDate,format="%a, %d %b %Y %H:%M:%S")})
+  rss$link <- rss$origLink
+return(rss)
 }
 
 
